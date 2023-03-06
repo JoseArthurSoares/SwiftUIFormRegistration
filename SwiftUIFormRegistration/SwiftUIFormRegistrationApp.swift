5917dd87f77d9d418e88e7b6c48a1f42b32a03cb
Arthur Soares <arthursoares.js@gmail.com>
2023-03-06 11:06:23 -0300

    Initial Commit

---
+++ SwiftUIFormRegistration/SwiftUIFormRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormRegistrationApp.swift
+//  SwiftUIFormRegistration
+//
+//  Created by Arthur Soares on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
